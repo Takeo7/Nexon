b7aa28076b4ac7fa043e472fb1fdda1563d0dcf0
Eduardo Wagener <eduardowagener@gmail.com>
2018-09-08 19:19:35 +0200

    Added UI asset Updated Main Menu UI

---
+++ Assets/BGMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BGMovement : MonoBehaviour {
+
+	public RawImage img;
+	public float speed;
+
+	private void Update()
+	{
+		Rect rect = img.uvRect;
+		rect.x = Mathf.Sin(1 * speed*Time.time);
+		img.uvRect = rect;
+	}
+}

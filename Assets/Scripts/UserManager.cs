93b93db67afb7d449b8402271bb7bbcd86cb8f15
Payno <payno@concanogames.com>
2018-12-04 15:05:31 +0100

    NullReference Fixed

--- Assets/Scripts/UserManager.cs
+++ Assets/Scripts/UserManager.cs
@@ -31,19 +31,15 @@ public class UserManager : MonoBehaviour {
 #if UNITY_EDITOR
     private void Update()
     {
-        if( inputPassword.isFocused && Input.GetKeyDown( KeyCode.Tab ) )
+        if( inputPassword != null && inputPassword.isFocused && Input.GetKeyDown( KeyCode.Tab ) )
         {
-            if ( inputPassword2.IsActive())
+            if ( inputPassword2 != null && inputPassword2.IsActive())
                 inputPassword2.Select();
 
         }
-        if( inputUsuario.isFocused && Input.GetKeyDown( KeyCode.Tab ) )
+        if( inputUsuario != null && inputUsuario.isFocused && Input.GetKeyDown( KeyCode.Tab ) )
         {
             inputPassword.Select();
-        }
-        if( Input.GetKeyDown( KeyCode.KeypadEnter) || Input.GetKeyDown( KeyCode.Return ) )
-        {
-            GotoLogin();
         }
     }
 #endif
